28b1cd40894845525e428a959b4745e247ed8566
Bruce Hill <bhill@slac.stanford.edu>
2019-06-01 08:32:15 -0700

    Minor cleanup of launcher.

--- launch_loadServer.sh
+++ launch_loadServer.sh
@@ -17,14 +17,17 @@ if [ ! -e "$PYPROCMGR" ]; then
 	echo "Error: pyProcMgr.py not found!"
 	exit 1
 fi
-echo PYPROCMGR = $PYPROCMGR_DIR 
+echo PYPROCMGR = $PYPROCMGR
 
 TOP=`readlink -f $(dirname ${BASH_SOURCE[0]})`
 echo TOP = $TOP
 
+echo "loadServers being launched w/o console log files."
+echo "There's another version of this script in stressTestClients.git that logs to test dir hierarchy."
+
 cd $TOP
 $PYPROCMGR -c 10 $LOADSERVER \
-	'-m DELAY=0.010,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10 -d db/drive_100Counters.db'
+	'-m DELAY=0.007,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10 -d db/drive_100Counters.db'
 
 
 #	'-m DELAY=0.017,P=PVA:GW:TEST:$PYPROC_ID:,NELM=10'	\
